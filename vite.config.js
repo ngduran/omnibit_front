de3e4ed2ae9167e611371820c98f0ad3205de31f
ngduran <norbertogarciaduran@gmail.com>
2026-08-16 20:14:32 -0300

    Config pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
-  ],  
+  ],
+  base: "./",
   test: {
     globals: true,
     environment: 'jsdom',
