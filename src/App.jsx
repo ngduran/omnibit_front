d1eb9877270d75ec5e6f27b38f01dca07e341f2e
ngduran <norbertogarciaduran@gmail.com>
2026-08-24 00:36:57 -0300

    Configurações - caminho

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
       <UIProvider> {/* Envolvendo a aplicação */}
         <AppToaster />
         
-        <BrowserRouter>
+        <BrowserRouter basename="/omnibit_front">
           <Routes>
             {/* Rota Pública */}
             <Route path="/login" element={<Login />} />
