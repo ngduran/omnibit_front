db745e8294188d83e14c66f6bc35d6487a1d3d1e
ngduran <norbertogarciaduran@gmail.com>
2026-09-05 16:34:05 -0300

    Ajuste de configuração

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -6,10 +6,14 @@ const ID_AUCT = localStorage.getItem('NGROK_ID_AUCT') || '';
 // CHAVE DE CONTROLE: Alterne para 'false' para usar localhost
 const USE_NGROK = true; 
 
-const NGROK_URL = (id) => `https://${id}.ngrok-free.app`;
+const NGROK_URL = (id) => `${id}.ngrok-free.app`;
 
 // Pega dinamicamente o endereço da barra de URL ('localhost' no PC ou '192.168.1.4' no celular)
 const currentHost = window.location.hostname;
+
+console.log("==============================================");
+console.log( currentHost );
+console.log("==============================================");
 
 // export const ENV = {
 //   // Constrói as URLs base conforme sua lógica de desenvolvimento
@@ -17,8 +21,7 @@ const currentHost = window.location.hostname;
 //   AUCTORITAS_API: USE_NGROK && ID_AUCT ? NGROK_URL(ID_AUCT) : "http://127.0.0.1:8085",
 // };
 
-export const ENV = {
-  // Se USE_NGROK for falso, ele usa automaticamente o IP/host atual da rede
+export const ENV = {  
   NXD_API: USE_NGROK && ID_NXD ? NGROK_URL(ID_NXD) : `http://${currentHost}:8080`,
   AUCTORITAS_API: USE_NGROK && ID_AUCT ? NGROK_URL(ID_AUCT) : `http://${currentHost}:8085`,
 };
